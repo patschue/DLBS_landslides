d5acd250fda6946e1f1d903f72a99861d1dae9d1
patrick.schuermann <patrick.schuermann@students.fhnw.ch>
2023-10-16 17:11:03 +0200

    results after run

--- helpers/training.py
+++ helpers/training.py
@@ -2,39 +2,6 @@ from torchmetrics.classification import MulticlassF1Score
 from torchmetrics.classification import BinaryF1Score
 import torch
 
-def train_deeplab(dataloader, model, loss_fn, optimizer, device):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    train_loss, f1_value = 0, 0
-    model.train()
-
-    f1_score = MulticlassF1Score(num_classes=2, average='none').to(device)
-
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
-        y = y.squeeze(1)
-        y = y.long()  
-
-        # Compute prediction error
-        pred = model(X)['out']
-        loss = loss_fn(pred, y)
-        train_loss += loss_fn(pred, y).item()
-
-        f1_value += f1_score(pred, y)
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    train_loss /= num_batches
-    f1_value /= num_batches
-
-    f1_value_background, f1_value_landslide = f1_value.tolist()
-    return train_loss, f1_value_background, f1_value_landslide
-
-
-
 def train_binary(dataloader, model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
